18bd29d42346a529c1533a8ad2b6a254340ff004
kamitobi <ssuyuanchang@outlook.com>
2025-06-12 04:51:06 +0800

    updated images

--- src/GUI/GameInfo.java
+++ src/GUI/GameInfo.java
@@ -21,7 +21,7 @@ public class GameInfo extends JDialog {
         name = new JLabel("24点");
         name.setFont(new Font("微软雅黑", Font.BOLD, 20));
         author = new JLabel("By 张思远，程亚迪");
-        description = new JLabel("Java课程设计    ver 0.8.4-alpha");
+        description = new JLabel("Java课程设计    ver 0.9.0-beta");
     }
     private void setLayout(){
         var gl = new GroupLayout(getContentPane());
