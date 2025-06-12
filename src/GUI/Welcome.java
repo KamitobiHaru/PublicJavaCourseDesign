57c0723cb4f248e94964dd2814172ddbb4a51c96
kamitobi <ssuyuanchang@outlook.com>
2025-06-13 01:35:06 +0800

    updated images

--- src/GUI/Welcome.java
+++ src/GUI/Welcome.java
@@ -7,9 +7,8 @@ import java.awt.Image;
 import java.awt.event.*;
 import java.util.Objects;
 import javax.swing.*;
-import static javax.swing.GroupLayout.DEFAULT_SIZE;
+
 public class Welcome extends JFrame{
-    private JButton continueButton;
     private JLabel words;
     private ImageIcon imageIcon;
     public Welcome(){
@@ -17,13 +16,13 @@ public class Welcome extends JFrame{
     }
     private void initUI(){
         setWindowBasics();
-        setButton();
         setWelcomeWord();
-        setLayout();
+        setAction();
+        add(words);
     }
     private void setWindowBasics(){
         setTitle("24点");
-        setSize(652,375);
+        setSize(720,480);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         var icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/GUI/images/icon.png")));
@@ -36,10 +35,11 @@ public class Welcome extends JFrame{
         imageIcon = new ImageIcon(
                 Objects.requireNonNull(getClass().getResource("/GUI/images/TitleImage.png"))
         );
-        Image titleImage = imageIcon.getImage().getScaledInstance(521, 300, Image.SCALE_SMOOTH);
+        Image titleImage = imageIcon.getImage().getScaledInstance(720, 480, Image.SCALE_SMOOTH);
         ImageIcon imageIcon = new ImageIcon(titleImage);
+        getContentPane().setBackground(new Color(248, 241, 243));
         words.setIcon(imageIcon);
-        setLayout();
+        words.setHorizontalAlignment(SwingConstants.CENTER);
         this.addWindowStateListener((WindowEvent ignoredEvent) -> titleImageResize());
         this.addComponentListener(new ComponentAdapter(){
             @Override
@@ -50,23 +50,20 @@ public class Welcome extends JFrame{
     }
     // resize imageIcon when component (JLabel) resized
     private void titleImageResize(){
-        words.setSize((int)(getWidth() * 0.8), (int)(getHeight() * 0.8));
+        words.setSize((getContentPane().getWidth()), (getContentPane().getHeight()));
         double width, height;
         width = words.getWidth();
         height = words.getHeight();
-        if (width > height * 1500 / 680)
-            width = height * 1500 / 680;
-        else if (width < height * 1500 / 680)
-            height = width*680/1500;
+        if (width > height * 720 / 480)
+            width = height * 720 / 480;
+        else if (width < height * 720 / 480)
+            height = width*480/720;
         Image titleImage = imageIcon.getImage().getScaledInstance((int)width, (int)height, Image.SCALE_SMOOTH);
         ImageIcon newIcon = new ImageIcon(titleImage);
         words.setIcon(newIcon);
     }
     // initialize start button
-    private void setButton(){
-        continueButton = new JButton("开始游戏 (Enter)");
-        continueButton.setBackground(Color.GRAY);
-        continueButton.setForeground(Color.WHITE);
+    private void setAction(){
         var continueAction = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e){
@@ -74,32 +71,15 @@ public class Welcome extends JFrame{
                 var ignored = new StartGameWindow();
             }
         };
-        continueButton.addActionListener(continueAction);
+        getContentPane().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                continueAction.actionPerformed(null);
+            }
+        });
         // bind the button with enter
-        continueButton.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+        words.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
             .put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER,0),"continueAction");
-        continueButton.getActionMap().put("continueAction", continueAction);
+        words.getActionMap().put("continueAction", continueAction);
     }
-    // use GroupLayout to set layout
-    private void setLayout(){
-        var pane = getContentPane();
-        var gl = new GroupLayout(pane);
-        pane.setLayout(gl);
-        gl.setAutoCreateContainerGaps(true);
-        gl.setAutoCreateGaps(true);
-        gl.setVerticalGroup(
-            gl.createSequentialGroup()
-                .addGap(0,0,Integer.MAX_VALUE)
-                .addComponent(words,DEFAULT_SIZE,DEFAULT_SIZE,Integer.MAX_VALUE)
-                .addComponent(continueButton,DEFAULT_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
-                .addGap(0,0,Integer.MAX_VALUE)
-        );
-        gl.setHorizontalGroup(
-            gl.createParallelGroup(GroupLayout.Alignment.CENTER)
-                .addGap(0,0,Integer.MAX_VALUE)
-                .addComponent(words)
-                .addComponent(continueButton)
-                .addGap(0,0,Integer.MAX_VALUE)
-        );
-    } 
 }
